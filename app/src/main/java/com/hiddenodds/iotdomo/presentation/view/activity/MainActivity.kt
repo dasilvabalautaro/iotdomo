e651bba5fecf4f9e99659ea6cf02beba68b091ad
arturosilva <david.arturo.silva@hotmail.com>
2018-01-09 22:05:05 -0400

    init project

---
+++ app/src/main/java/com/hiddenodds/iotdomo/presentation/view/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.hiddenodds.iotdomo.presentation.view.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.hiddenodds.iotdomo.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
